23406413918f84ea8462578060d056fa40b49fc6
Soner Karabulut <84913012+BuzOkcu@users.noreply.github.com>
2022-12-10 23:06:51 +0300

    Add files via upload

---
+++ cprojeckt01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+    char name[6] = "Soner";
+    char surname[10] = "Karabulut";
+    char city[8] = "Antalya";
+    printf("%s\t" , name);
+    printf("%s\t" , surname);
+    printf("%s" , city);
+    getch();
+}
