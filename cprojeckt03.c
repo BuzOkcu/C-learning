73a8de610e21c3818fb7334fad86fef6d4e86272
Soner Karabulut <84913012+BuzOkcu@users.noreply.github.com>
2022-12-11 13:48:19 +0300

    Add files via upload

---
+++ cprojeckt03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+    int x;
+    int y;
+    printf("Enter a number : ");
+    scanf("%d" , &x);
+    printf("Enter a number : ");
+    scanf("%d" , &y);
+
+    printf("Sum of numbers : %d" , x + y);
+    getch();
+}
